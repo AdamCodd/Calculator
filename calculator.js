894eb641ad2a026d345c442dd70ec2af666ab5a5
Adam <49641859+AdamCodd@users.noreply.github.com>
2022-08-09 19:40:15 +0200

    Code refactoring

--- calculator.js
+++ calculator.js
@@ -1,15 +1,30 @@
+const showDigits = document.querySelector('.result');
+const showHistory = document.querySelector('.history');
+const allDigits = document.querySelectorAll('.digits button[data-key]');
+const operators = document.querySelectorAll('.operators button');
+const equal = document.querySelector('.equal');
+const clear = document.querySelector('.clear');
+const erase = document.querySelector('.delete');
+const dot = document.querySelector('.dot');
+
+let firstNumbers, secondNumbers, solution;
+let op, isFirstOp;
+
+function reset() {
+    firstNumbers = secondNumbers = solution = op = showDigits.textContent = showHistory.textContent = '';
+    isFirstOp = true;
+}
+reset();
+
 function add(a, b) {
     return a + b;
 }
-
 function substract(a, b) {
     return a - b;
 }
-
 function multiply(a, b) {
     return a * b;
 }
-
 function divide(a, b) {
     return a / b;
 }
@@ -17,60 +32,96 @@ function divide(a, b) {
 function operate(op, a, b) {
     a = Number.parseFloat(a, 10);
     b = Number.parseFloat(b, 10);
-    let result;
     switch (op) {
-        case 'add':
-            result = add(a, b);
-            break;
-        case 'substract':
-            result = substract(a, b);
-            break;
-        case 'multiply':
-            result = multiply(a, b);
-            break;
-        case 'divide':
-            result = divide(a, b);
-            break;
+        case '+':
+            return add(a, b);
+        case '-':
+            return substract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            if (b === 0) return '';
+            else return divide(a, b);
         default:
             console.error('Operator error!', op);
     }
-    return result;
 }
 
-const showDigits = document.querySelector('.result');
-const showHistory = document.querySelector('.history');
-const allDigits = document.querySelectorAll('.digits button');
-const operators = document.querySelectorAll('.operators button');
-const equal = document.querySelector('.equal');
-const clear = document.querySelector('.clear');
-const erase = document.querySelector('.delete');
+function addNumbers(num) {
+    if (showDigits.textContent === '0') showDigits.textContent = '';
+    showDigits.textContent += num;
+}
 
-let numbers, firstNumbers, secondNumbers, solution;
-let op, newOp, isFirstOp;
+allDigits.forEach(digit => {
+    digit.addEventListener('click', () => {
+        addNumbers(digit.textContent);
+    });
+});
 
-function reset() {
-    firstNumbers = secondNumbers = solution = op = newOp = showHistory.textContent = '';
-    numbers = '0';
+dot.addEventListener('click', () => {
+    if (!showDigits.textContent.includes('.')) showDigits.textContent += '.';
+})
+
+erase.addEventListener('click', () => {
+    if (showDigits.textContent === '0') showDigits.textContent = '';
+    showDigits.textContent = showDigits.textContent.toString().slice(0, -1);
+});
+
+function operation(operator) {
+    if (isFirstOp === false) evaluate();
+    if (isFirstOp === true) {
+        op = operator;
+        firstNumbers = showDigits.textContent;
+        showHistory.textContent = `${firstNumbers} ${op}`;
+        showDigits.textContent = '';
+        isFirstOp = false;
+    }
+}
+
+function evaluate() {
+    if (isFirstOp === true) return;
+
+    secondNumbers = showDigits.textContent;
+
+    if (op === '/' && secondNumbers === "0") {
+        alert("Can't divide by 0 bro!");
+        return;
+    }
+
+    if (secondNumbers !== '') solution = operate(op, firstNumbers, secondNumbers);
+    Number.isInteger(solution) ? '' : solution = Number(solution.toFixed(3));
+    showDigits.textContent = solution;
+
+    showHistory.textContent = `${firstNumbers} ${op} ${secondNumbers} =`;
+    secondNumbers = '';
     isFirstOp = true;
 }
-reset();
 
+operators.forEach(operator => {
+    operator.addEventListener('click', () => {
+        operation(operator.textContent);
+    });
+});
+
+equal.addEventListener('click', evaluate);
+
+clear.addEventListener('click', () => {
+    showDigits.textContent = '0';
+    reset();
+});
+
+// Keyboard support
 window.addEventListener('keydown', (e) => {
-    if (e.key === "\'" || e.key === "\"") return;
-    const key = document.querySelector(`button[data-key="${e.key}"]`);
-    console.log(key, e.key);
-    if (!key) return;
     if (e.key === "=") {
         let clickEqual = new Event('click');
         equal.dispatchEvent(clickEqual);
     }
     else if (e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/") {
-        let clickSign = new Event('click');
-        operators.forEach(operator => {
-            if (operator.dataset.key === e.key) {
-                operator.dispatchEvent(clickSign);
-            }
-        })
+        operation(e.key);
+    }
+    else if (e.key === ".") {
+        let clickDot = new Event('click');
+        dot.dispatchEvent(clickDot);
     }
     else if (e.key === "Backspace") {
         let clickErase = new Event('click');
@@ -80,99 +131,8 @@ window.addEventListener('keydown', (e) => {
         let clickClear = new Event('click');
         clear.dispatchEvent(clickClear);
     }
-    else {
-        let clickDigit = new Event('click');
-        allDigits.forEach(digit => {
-            if (digit.dataset.key === e.key) {
-                digit.dispatchEvent(clickDigit);
-            }
-        });
+    else if (e.key >= 0 && e.key <= 9) {
+        addNumbers(e.key);
     }
-});
 
-allDigits.forEach(digit => {
-    digit.addEventListener('click', () => {
-        if (!digit.classList.contains('equal') && !digit.classList.contains('dot')) {
-            if (numbers === '0') numbers = '';
-            numbers += digit.textContent;
-            showDigits.textContent = numbers;
-        }
-
-        if (digit.classList.contains('dot') && !numbers.includes('.') && numbers !== '') {
-            numbers += ".";
-            showDigits.textContent = numbers;
-        }
-    });
-});
-
-erase.addEventListener('click', () => {
-    if (solution === '') {
-        let arr = numbers.split('');
-        numbers = arr.slice(0, -1).join('');
-        showDigits.textContent = numbers;
-    }
-    else {
-        let arr = solution.toString().split('');
-        solution = Number(arr.slice(0, -1).join(''));
-        firstNumbers = showDigits.textContent = solution;
-    }
-});
-
-operators.forEach(operator => {
-    operator.addEventListener('click', () => {
-        if (isFirstOp === true) {
-            op = operator.className;
-            if (firstNumbers === '') firstNumbers = +numbers;
-            showHistory.textContent = `${firstNumbers} ${operator.dataset.id}`;
-            numbers = secondNumbers = '';
-        }
-
-        if (isFirstOp === false) {
-            secondNumbers = +numbers;
-            // console.log(op, `First: ${firstNumbers}`, `Second: ${secondNumbers}`);
-            newOp = operator.className;
-            if (op === 'divide' && secondNumbers == '0') {
-                showDigits.textContent = "Can't divide by 0 bro!";
-                numbers = secondNumbers = '';
-                op = newOp;
-            }
-            else {
-                solution = operate(op, firstNumbers, secondNumbers);
-                Number.isInteger(solution) ? '' : solution = Number(solution.toFixed(3));
-                firstNumbers = showDigits.textContent = solution;
-                showHistory.textContent = `${firstNumbers} ${operator.dataset.id}`;
-                numbers = secondNumbers = '';
-                op = newOp;
-            }
-        }
-        isFirstOp = false;
-
-    });
-});
-
-equal.addEventListener('click', () => {
-    isFirstOp = true;
-    secondNumbers = +numbers;
-
-    if (op !== '' && firstNumbers !== '' && secondNumbers !== '') {
-        const getOpSign = [...document.getElementsByClassName(op)][0].dataset.id;
-        showHistory.textContent = `${firstNumbers} ${getOpSign} ${secondNumbers} ${equal.textContent}`;
-        if (op === 'divide' && secondNumbers == '0') {
-            showDigits.textContent = "Can't divide by 0 bro!";
-            numbers = secondNumbers = '';
-        }
-        else {
-            solution = operate(op, firstNumbers, secondNumbers);
-            Number.isInteger(solution) ? '' : solution = Number(solution.toFixed(3));
-            firstNumbers = showDigits.textContent = solution;
-
-            //console.log("Solution: " + solution, "First: " + firstNumbers, "Second: " + secondNumbers, "Op:" + op);
-            secondNumbers = op = '';
-        }
-    }
-});
-
-clear.addEventListener('click', () => {
-    showDigits.textContent = '0';
-    reset();
 });
