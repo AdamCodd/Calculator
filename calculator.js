5d77aac098829b9a0173f3d4eb1d07e39a2b1858
AdamCodd <49641859+AdamCodd@users.noreply.github.com>
2023-12-08 00:53:54 +0100

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -26,9 +26,8 @@ function resetCalculator() {
 // Update display screen
 function updateDisplay() {
     displayCurrent.textContent = currentInput;
-    displayHistory.textContent = previousInput;
+    displayHistory.textContent = previousInput + ' ' + (currentOperator ? currentOperator : '');
 }
-
 // Basic arithmetic functions
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
@@ -51,10 +50,14 @@ function operate(operator, a, b) {
 // Handle digit button click
 function inputDigit(digit) {
     if (resetScreen) {
-        currentInput = digit;
+        currentInput = digit === '0' ? '0' : digit;
         resetScreen = false;
     } else {
-        currentInput = currentInput === '0' ? digit : currentInput + digit;
+        if (currentInput === '0' && digit !== '0') {
+            currentInput = digit;
+        } else if (currentInput !== '0') {
+            currentInput += digit;
+        }
     }
     updateDisplay();
 }
@@ -98,16 +101,25 @@ function inputOperator(operator) {
 
 // Perform calculation and update display
 function evaluate() {
-    if (currentOperator === null || resetScreen) return;
+    if (!currentOperator || resetScreen) return;
     const calculation = operate(currentOperator, previousInput, currentInput);
     if (calculation === null) {
         alert("Can't divide by zero!");
+        resetCalculator();
         return;
     }
-    currentInput = String(calculation);
+    currentInput = formatResult(calculation);
+    previousInput = '';
     currentOperator = null;
     resetScreen = true;
     updateDisplay();
+}
+
+function formatResult(result) {
+    if (result.toString().length > 10) { // Adjust the length as needed
+        return result.toPrecision(10); // Use scientific notation for large numbers
+    }
+    return String(result);
 }
 
 // Adding event listeners to buttons
@@ -118,15 +130,24 @@ clearButton.addEventListener('click', resetCalculator);
 deleteButton.addEventListener('click', deleteDigit);
 decimalButton.addEventListener('click', inputDecimal);
 
-// Initialize calculator
-resetCalculator();
-
 // Keyboard support
 window.addEventListener('keydown', (e) => {
     if (e.key.match(/[0-9]/)) inputDigit(e.key);
     else if (e.key === '.') inputDecimal();
-    else if (e.key.match(/[\+\-\*\/]/)) inputOperator(e.key);
+    else if (e.key.match(/[\+\*\/]/)) inputOperator(e.key);
     else if (e.key === 'Enter' || e.key === '=') evaluate();
     else if (e.key === 'Backspace') deleteDigit();
     else if (e.key === 'Escape') resetCalculator();
+    else if (e.key === '-') {
+        if (currentInput === '' || currentInput === '0' || resetScreen) {
+            currentInput = '-';
+            resetScreen = false;
+            updateDisplay();
+        } else {
+            inputOperator('-');
+        }
+    }
 });
+
+// Initialize calculator
+resetCalculator();
